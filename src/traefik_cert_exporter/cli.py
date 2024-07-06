c824e5175217323b871925f1eb4d02fe226e73c6
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2024-07-06 15:48:19 -0600

    feat: add CLI function

---
+++ src/traefik_cert_exporter/cli.py
@@ -0,0 +1,20 @@
+"""Traefik certificate exporter CLI."""
+
+import click
+
+from .exporter import export_certificates
+
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
+
+
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.argument("storage", type=click.Path(exists=True))
+@click.argument("output", type=click.Path())
+def export_certs(storage, output):
+    """Export certificates from Traefik's STORAGE file to a specified OUTPUT directory.
+
+    \b
+    STORAGE is the location where Traefik saves the ACME certificates.
+    OUTPUT is the path to a directory where the certificate files will be exported to.
+    """
+    export_certificates(storage, output)
